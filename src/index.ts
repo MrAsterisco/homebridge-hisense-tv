c6a8a0951c441fdc3df06f80ffdeb122c8a770ee
LeLunZ <31982496+LeLunZ@users.noreply.github.com>
2024-06-09 21:37:38 +0200

    Fix imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
-import { HiSenseTVPlatform } from './platform';
+import { PLATFORM_NAME } from './settings.js';
+import { HiSenseTVPlatform } from './platform.js';
 
 /**
  * This method registers the platform with Homebridge
